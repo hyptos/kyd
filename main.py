ba95bb75995d64b848e3056b62e1b6ad14b9cfb9
p0907931 <devnull@localhost>
2015-01-19 15:41:21 +0100

    ajout des tests basiques pour dropbox

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+import datetime
 from BeautifulSoup import BeautifulSoup
 import requests
 
@@ -6,7 +8,7 @@ __author__ = 'antoine'
 import unittest
 import dropbox
 import provider
-
+import timeit
 
 class TestConnexionFunctions(unittest.TestCase):
     # def test_connexionDropbox(self):
@@ -25,9 +27,35 @@ class TestConnexionFunctions(unittest.TestCase):
     def test_connexionDropboxWithToken(self):
         p = provider.Provider('dropbox')
         client = dropbox.client.DropboxClient(p.token)
-        print client.account_info()
         self.assertTrue(len(client.account_info()) >= 3)
 
+    def test_sendFileDropbox(self):
+
+        # Upload the file
+        p = provider.Provider('dropbox')
+
+        client = dropbox.client.DropboxClient(p.token)
+        f = open('working-draft.txt', 'rb')
+        nowUpload = datetime.datetime.now()
+        response = client.put_file('/testText10k_01.txt', f)
+        afterUpload = datetime.datetime.now()
+        diff = afterUpload - nowUpload
+        print "Upload time for 10k file " + diff.__str__()
+
+
+        # Get the file
+        nowDownload = datetime.datetime.now()
+        f, metadata = client.get_file_and_metadata('/testText10k_01.txt')
+        afterDownload = datetime.datetime.now()
+        diff = afterDownload - nowDownload
+        print "Download time for 10k file " + diff.__str__()
+
+        out = open('testText10k_01.txt', 'wb')
+        out.write(f.read())
+        out.close()
+
+        response = client.file_delete('/testText10k_01.txt')
+        self.assertTrue(response['is_deleted'])
 
 if __name__ == '__main__':
     unittest.main()
