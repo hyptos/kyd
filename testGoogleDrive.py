28c8331123799dd333a0d83086fca4a48bcc6bfb
Carole Bonfre <carole.bonfre@etu.univ-lyon1.fr>
2015-01-27 16:53:48 +0100

    Connexion google drive sans demander le token à chaque fois + download + delete non testé (trop de connexions...)

--- testGoogleDrive.py
+++ testGoogleDrive.py
@@ -1,11 +1,52 @@
+import apiclient
 import googleapiclient
 import pprint
 import httplib2
-import apiclient.discovery
-import apiclient.http
-import oauth2client.client
+from oauth2client.file import Storage
+from apiclient.discovery import build
+from oauth2client.client import OAuth2WebServerFlow
 import provider
+from apiclient import errors
 
+
+def delete_file(service, file_id):
+  """Permanently delete a file, skipping the trash.
+
+  Args:
+    service: Drive API service instance.
+    file_id: ID of the file to delete.
+  """
+  try:
+    service.files().delete(fileId=file_id).execute()
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+def download_file(service, drive_file):
+    """Download a file's content.
+
+    Args:
+    service: Drive API service instance.
+    drive_file: Drive File instance.
+
+    Returns:
+    File if successful, None otherwise.
+    """
+    download_url = drive_file.get('downloadUrl')
+    if download_url:
+        resp, content = service._http.request(download_url)
+        if resp.status == 200:
+            print 'Status: %s' % resp
+            out = open('testText10k_02.txt', 'wb')
+            out.write(content)
+            out.close()
+            return out
+        else:
+            print 'An error occurred: %s' % resp
+            return None
+    else:
+        # The file doesn't have any content stored on Drive.
+        return None
 
 p = provider.Provider('googledrive')
 
@@ -25,18 +66,28 @@ MIMETYPE = 'text/plain'
 TITLE = 'My New Text Document'
 DESCRIPTION = 'A shiny new text document about lorem ipsum.'
 
-# Perform OAuth2.0 authorization flow.
-flow = oauth2client.client.flow_from_clientsecrets(CLIENT_SECRETS, OAUTH2_SCOPE)
-flow.redirect_uri = oauth2client.client.OOB_CALLBACK_URN
-authorize_url = flow.step1_get_authorize_url()
-print 'Go to the following link in your browser: ' + authorize_url
-code = raw_input('Enter verification code: ').strip()
-credentials = flow.step2_exchange(code)
+# Path to the crendentials
+CRED_FILENAME = 'credentials'
+### For storing token
+storage = Storage(CRED_FILENAME)
+if not storage.get():
+    # Run through the OAuth flow and retrieve authorization code
+    flow = OAuth2WebServerFlow(p.app_key, p.app_secret, OAUTH2_SCOPE, 'urn:ietf:wg:oauth:2.0:oob')
+    authorize_url = flow.step1_get_authorize_url()
+    print 'Go to the following link in your browser: ' + authorize_url
+    code = raw_input('Enter verification code: ').strip()
+    credentials = flow.step2_exchange(code)
+    ### Storing access token and a refresh token in CRED_FILENAME
+    storage.put(credentials)
+else:
+    ### Getting access_token,expires_in,token_type,Refresh_toke info from CRED_FILENAME to 'credentials'
+    credentials = storage.get()
+
 
 # Create an authorized Drive API client.
 http = httplib2.Http()
 credentials.authorize(http)
-drive_service = apiclient.discovery.build('drive', 'v2', http=http)
+drive_service = build('drive', 'v2', http=http)
 
 # Insert a file. Files are comprised of contents and metadata.
 # MediaFileUpload abstracts uploading file contents from a file on disk.
@@ -54,6 +105,7 @@ body = {
 
 # Perform the request and print the result.
 new_file = drive_service.files().insert(body=body, media_body=media_body).execute()
-pprint.pprint(new_file)
 
+download_file(drive_service, new_file)
 
+delete_file(drive_service, new_file)
