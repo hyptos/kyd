6132b298f0bf55392c8902870bc4cc2b267e1697
p0907931 <devnull@localhost>
2015-01-28 14:44:26 +0100

    Documentation de S3 + licence + README

---
+++ README.md
@@ -0,0 +1,36 @@
+KYP
+==============
+
+Authors :
+
+    Carole Bonfré
+    
+    Antoine Martin
+
+Tutors : 
+
+    Eddy Caron
+    
+    Yves Caniou 
+ 
+University Claude Bernard Lyon 1 
+
+Get started
+===============
+
+Install dependencies :
+
+    pip install -r requirements.txt
+    
+Todo
+===============
+
+
+Tutorial
+===============
+
+How to use the application
+
+Licence
+===============
+See the LICENCE file
--- testS3.py
+++ testS3.py
@@ -6,39 +6,87 @@ from boto.s3.connection import S3Connection, Location
 __author__ = 'antoine'
 
 
-def deleteFile(bucket, cle):
-    print 'Delete du bucket'
-    bucket.delete_key(cle)
+def deleteFile(bucket, key):
+    """Delete a file on S3.
+
+    Args:
+    bucket: bucket instance.
+    key: Key to delete.
+
+    Returns:
+    None.
+    """
+    bucket.delete_key(key)
 
 
-def uploadFile(nomFichier, nomCle, nomBucket):
-    print 'Upload du fichier'
-    b = getConnexion().get_bucket(nomBucket)
+def uploadFile(fileName, keyName, bucketName):
+    """Upload a file to a specific bucket with a specific KeyName.
+
+    Args:
+    bucketName: name of the bucket where the file will be stored.
+    keyName: Key where we store the file
+    fileName: Name of the file on S3.
+
+    Returns:
+    None.
+    """
+    b = getConnexion().get_bucket(bucketName)
     k = getKey(b)
-    k.key = nomCle
-    k.set_contents_from_filename(nomFichier)
-    print 'Fin upload'
+    k.key = keyName
+    k.set_contents_from_filename(fileName)
 
 
-def downloadFile(bucket, cle, nouveauNomFichier):
-    print 'Download du fichier'
-    key_we_know_is_there = bucket.get_key(cle, validate=False)
-    res = key_we_know_is_there.get_contents_to_filename(nouveauNomFichier)
-    print 'Fin download'
+def downloadFile(bucket, key, newFileName):
+    """Download a file and save it.
+
+    Args:
+    bucket: bucket instance.
+    key: Drive File instance.
+    newFileName: Name of the file.
+
+    Returns:
+    File if successful, None otherwise.
+    """
+    key_we_know_is_there = bucket.get_key(key, validate=False)
+    return key_we_know_is_there.get_contents_to_filename(newFileName)
 
 
 def getConnexion():
+    """Log an instance for S3.
+
+    Args:.
+
+    Returns:
+    connection instance of S3  if successful, None otherwise.
+    """
     p = provider.Provider('amazon')
     return S3Connection(p.app_key, p.app_secret)
 
 
-def getKey(cle):
-    return Key(cle)
+def getKey(key):
+    """return a Key for S3.
+
+    Args:.
+        key: key to return
+
+    Returns:
+    Key instance  if successful, None otherwise.
+    """
+    return Key(key)
 
 
-def creationBucket(nomBucket, locationGeo):
+def createBucket(bucketName, locationGeo):
+    """Create a bucket on S3 for a zone.
+
+    Args:.
+        bucketName: name of the bucket
+        locationGeo: enum of boto.Location
+
+    Returns:
+    Bucket instance if successful, None otherwise.
+    """
     b = getConnexion()
-    return b.create_bucket(nomBucket, location=locationGeo)
+    return b.create_bucket(bucketName, location=locationGeo)
 
 
 if __name__ == '__main__':
@@ -46,7 +94,7 @@ if __name__ == '__main__':
     nomBucket = 'mdicbucket2015'
     cleBucket = 'testFichier01'
     nomFichier = 'working-draft.txt'
-    # b = creationBucket(nomBucket,Location.EU)
+    # b = createBucket(nomBucket,Location.EU)
     uploadFile(nomFichier, cleBucket, nomBucket)
     downloadFile(getConnexion().get_bucket(nomBucket), cleBucket, nomFichier)
     deleteFile(getConnexion().get_bucket(nomBucket), cleBucket)
