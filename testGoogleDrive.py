bdecec6d246ca5b0518e48064c2c54e7b5e7de09
Carole Bonfre <carole.bonfre@etu.univ-lyon1.fr>
2015-01-28 10:15:15 +0100

    Delete google drive ok + google drive modularisé

--- testGoogleDrive.py
+++ testGoogleDrive.py
@@ -9,17 +9,66 @@ import provider
 from apiclient import errors
 
 
-def delete_file(service, file_id):
-  """Permanently delete a file, skipping the trash.
+def init():
+    p = provider.Provider('googledrive')
 
-  Args:
-    service: Drive API service instance.
-    file_id: ID of the file to delete.
-  """
-  try:
-    service.files().delete(fileId=file_id).execute()
-  except errors.HttpError, error:
-    print 'An error occurred: %s' % error
+    # OAuth 2.0 scope that will be authorized.
+    # Check https://developers.google.com/drive/scopes for all available scopes.
+    OAUTH2_SCOPE = 'https://www.googleapis.com/auth/drive'
+
+    # Location of the client secrets.
+    CLIENT_SECRETS = 'client_secrets.json'
+
+    # Path to the crendentials
+    CRED_FILENAME = 'credentials'
+    ### For storing token
+    storage = Storage(CRED_FILENAME)
+    if not storage.get():
+        # Run through the OAuth flow and retrieve authorization code
+        flow = OAuth2WebServerFlow(p.app_key, p.app_secret, OAUTH2_SCOPE, 'urn:ietf:wg:oauth:2.0:oob')
+        authorize_url = flow.step1_get_authorize_url()
+        print 'Go to the following link in your browser: ' + authorize_url
+        code = raw_input('Enter verification code: ').strip()
+        credentials = flow.step2_exchange(code)
+        ### Storing access token and a refresh token in CRED_FILENAME
+        storage.put(credentials)
+    else:
+        ### Getting access_token,expires_in,token_type,Refresh_toke info from CRED_FILENAME to 'credentials'
+        credentials = storage.get()
+
+    # Create an authorized Drive API client.
+    http = httplib2.Http()
+    credentials.authorize(http)
+    drive_service = build('drive', 'v2', http=http)
+    return drive_service
+
+
+def upload_file(service, filePath, fileName, fileType, fileDescription):
+
+    # Metadata about the file.
+    MIMETYPE = 'text/plain'
+    TITLE = 'My New Text Document'
+    DESCRIPTION = 'A shiny new text document about lorem ipsum.'
+
+
+
+    # Insert a file. Files are comprised of contents and metadata.
+    # MediaFileUpload abstracts uploading file contents from a file on disk.
+    media_body = apiclient.http.MediaFileUpload(
+    filePath,
+    mimetype=fileType,
+    resumable=True
+    )
+
+    # The body contains the metadata for the file.
+    body = {
+    'title': fileName,
+    'description': fileDescription,
+    }
+
+    new_file = service.files().insert(body=body, media_body=media_body).execute()
+
+    return new_file
 
 
 def download_file(service, drive_file):
@@ -48,64 +97,30 @@ def download_file(service, drive_file):
         # The file doesn't have any content stored on Drive.
         return None
 
-p = provider.Provider('googledrive')
+
+def delete_file(service, file_id):
+    """Permanently delete a file, skipping the trash.
+
+    Args:
+    service: Drive API service instance.
+    file_id: ID of the file to delete.
+    """
+    try:
+        service.files().delete(fileId=file_id).execute()
+    except errors.HttpError, error:
+        print 'An error occurred: %s' % error
 
 
-# OAuth 2.0 scope that will be authorized.
-# Check https://developers.google.com/drive/scopes for all available scopes.
-OAUTH2_SCOPE = 'https://www.googleapis.com/auth/drive'
+def test_all():
 
-# Location of the client secrets.
-CLIENT_SECRETS = 'client_secrets.json'
+    drive_service = init()
 
-# Path to the file to upload.
-FILENAME = 'working-draft.txt'
+    DESCRIPTION = 'A shiny new text document about lorem ipsum.'
 
-# Metadata about the file.
-MIMETYPE = 'text/plain'
-TITLE = 'My New Text Document'
-DESCRIPTION = 'A shiny new text document about lorem ipsum.'
+    new_file = upload_file(drive_service, 'working-draft.txt', 'My New Text Document', 'text/plain', DESCRIPTION)
 
-# Path to the crendentials
-CRED_FILENAME = 'credentials'
-### For storing token
-storage = Storage(CRED_FILENAME)
-if not storage.get():
-    # Run through the OAuth flow and retrieve authorization code
-    flow = OAuth2WebServerFlow(p.app_key, p.app_secret, OAUTH2_SCOPE, 'urn:ietf:wg:oauth:2.0:oob')
-    authorize_url = flow.step1_get_authorize_url()
-    print 'Go to the following link in your browser: ' + authorize_url
-    code = raw_input('Enter verification code: ').strip()
-    credentials = flow.step2_exchange(code)
-    ### Storing access token and a refresh token in CRED_FILENAME
-    storage.put(credentials)
-else:
-    ### Getting access_token,expires_in,token_type,Refresh_toke info from CRED_FILENAME to 'credentials'
-    credentials = storage.get()
+    download_file(drive_service, new_file)
 
+    delete_file(drive_service, new_file['id'])
 
-# Create an authorized Drive API client.
-http = httplib2.Http()
-credentials.authorize(http)
-drive_service = build('drive', 'v2', http=http)
-
-# Insert a file. Files are comprised of contents and metadata.
-# MediaFileUpload abstracts uploading file contents from a file on disk.
-media_body = apiclient.http.MediaFileUpload(
-FILENAME,
-mimetype=MIMETYPE,
-resumable=True
-)
-
-# The body contains the metadata for the file.
-body = {
-'title': TITLE,
-'description': DESCRIPTION,
-}
-
-# Perform the request and print the result.
-new_file = drive_service.files().insert(body=body, media_body=media_body).execute()
-
-download_file(drive_service, new_file)
-
-delete_file(drive_service, new_file)
+test_all()
