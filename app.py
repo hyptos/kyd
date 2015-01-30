42da0d4f4aac63f379ce50586a67914570cb743f
p0907931 <devnull@localhost>
2015-01-30 11:15:35 +0100

    avancement dans les parametres

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ import argparse
 def init():
     print 'init'
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--init' ,type=init)
+parser = argparse.ArgumentParser(prog='KYP')
+parser.add_argument('-s', '--size', type=int, help='size of the test')
+parser.add_argument('-o', '--only', type=bool, help='test only this size of the test', default=False)
+parser.add_argument('-p', '--protocol', choices=['sdk', 'rest','all'], default='all')
+parser.add_argument('-t', '--transfert', choices=['inter', 'upload','download','upDown'], default='upDown')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 args = parser.parse_args()
 
 if __name__ == '__main__':
-    init()
+    print 'arg de size: ' + str(args.size)
+    print 'arg de only: ' + str(args.only)
+    print 'arg de protocol: ' + str(args.protocol)
+    print 'arg de transfert: ' + str(args.transfert)
